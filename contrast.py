6976b9783548337cc9a850a1af1cba3ddeacdfc3
AndreasPsy <158124593+AndreasPsy@users.noreply.github.com>
2024-04-10 19:00:58 -0700

    Update contrast.py
    Clarifying comments

--- contrast.py
+++ contrast.py
@@ -1,3 +1,8 @@
+#Reference DualFeed.py for explanations of start_cam(), cap_array(), and disp_array()
+#This file adds the contrast algorithm to dual feed, and will display a contrasted black and white video feed in addition to the black and white feed 
+    #Note: The contrast works by stretching brightness values across the full 255-value spectrum; 
+    #      ie it makes the lightest pixel 0 and the darkest 255 and then scales all other pixels appropriately
+
 import numpy as np
 from picamera2 import Picamera2
 from PIL import Image as im
@@ -6,28 +11,27 @@ import cv2
 
 def main():
     feed1 = start_cam(0)
-    ##feed2 = start_cam(1)
+    #feed2 = start_cam(1)
     
 
     while (True):
         arr1 = cap_array(feed1)
         arr1_con = contrast(arr1)
-        ##arr2 = cap_array(feed2)
-        ##arr3 = arr2 * 1.1
-        ##arr3 = arr3.astype('int8')
+        #arr2 = cap_array(feed2)
+        #arr2_con = contrast(arr2)
         
         disp_array('Feed 1', arr1)
-        ##disp_array('Feed 2', arr2)
-        ##disp_array('feed 3', arr3)
-        disp_array('Contrast', arr1_con)
+        #disp_array('Feed 2', arr2)
+        
+        disp_array('Contrast 1', arr1_con)
+        #disp_array('Contrast 2', arr2_con)
 
         
         if cv2.waitKey(1) == ord('q'):
             break
     
-    print(arr1.ndim)  
     feed1.stop()
-    ##feed2.stop()
+    #feed2.stop()
     
     
 def start_cam(camera):
