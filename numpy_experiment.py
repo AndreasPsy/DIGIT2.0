e97cb80fe9f42f33dfae68a5a8093d216e41adb1
AndreasPsy <158124593+AndreasPsy@users.noreply.github.com>
2024-04-10 19:04:21 -0700

    Update numpy_experiment.py
    Clarifying comments

--- numpy_experiment.py
+++ numpy_experiment.py
@@ -1,3 +1,6 @@
+#This file was just for messing around with video to numpy arrays. It starts a feed, converts it to an array, 
+# takes a picture directly from the feed(test.jpg) and then takes a picture from the array(convertedTest.jpg)
+
 import numpy as np
 from picamera2 import Picamera2
 from PIL import Image as im
@@ -9,8 +12,10 @@ def start_cam(camera):
     picam2 = Picamera2(camera)
     picam2.start()
 
-    array = picam2.capture_array("main")
-    picam2.capture_file("test.jpg")
+def main()
+    feed1 = start_cam(0)
+    array = feed1.capture_array("main")
+    feed1.capture_file("test.jpg")
     picture = im.fromarray(array)
     picture = picture.convert('RGB')
     picture.save('convertedTest.jpg')
